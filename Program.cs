4c63cf071b1e011665f5b029929536eb84386fbf
Suman Miah <suman@redlimesolutions.com>
2025-10-20 13:57:03 +0600

    AddCors

--- Program.cs
+++ Program.cs
@@ -1,37 +1,40 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Enable CORS
+// CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+    options.AddPolicy("Frontends", policy =>
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
+        policy.WithOrigins(
+            "https://qrcode.rldhaka.com", // no trailing slash
+            "http://localhost:3000",      // dev (http)
+            "https://localhost:3000"      // dev (https)
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+        // .AllowCredentials(); // only if you actually send cookies/Authorization header
     });
+
+    // Optional: a permissive DEV policy you can toggle via env
+    options.AddPolicy("AllowAllDev", policy =>
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-app.UseCors("AllowAll");
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// Pick ONE policy:
+app.UseCors("Frontends");       // use this in prod
+// if (app.Environment.IsDevelopment()) app.UseCors("AllowAllDev");
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseStaticFiles();
 
 app.Run();
